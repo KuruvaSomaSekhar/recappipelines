e63a545db93e4a920904fd875f413e3dc24d2137
Soma Sekhar Kuruva <somasekhar.kuruva@trianz.com>
2022-02-10 07:57:10 +0530

    added build steps

--- JenkinsBuild.groovy
+++ JenkinsBuild.groovy
@@ -4,17 +4,21 @@ pipeline {
     stages {
         stage("checkout-clone") {
             steps{
-                println "Here we are closing our source code"
+                println "Here we are cloning our source code"
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'recap', url: 'https://github.com/KuruvaSomaSekhar/privatecode.git']]])
+
             }
         }
         stage("build"){
             steps{
                 println "Here we build"
+                sh "mvn clean package"
             }
         }
         stage("upload"){
             steps{
                 println "Uplod to s3"
+                sh "ls -lart"
             }
         }
 
