2a2ad59ef7eb474e593c82f314d86963299a001a
Soma Sekhar Kuruva <somasekhar.kuruva@trianz.com>
2022-02-10 08:16:00 +0530

    added deployment file

---
+++ JenkinsfileDeploy.groovy
@@ -0,0 +1,17 @@
+//declarative pipeline
+pipeline {
+    agent any
+    parameters { string(name: 'projectName', defaultValue: 'recapbuild', description: '')
+                 string(name: 'buildNumber', defaultValue: '', description: '')
+                 string(name: 'branchName', defaultValue: 'master', description: '')
+    
+     }
+     stages{
+         stage("download-artifacts"){
+             steps {
+                 println "cloing artifacts"
+                 sh "aws s3 cp s3://devops09art/${projectName}/${branchName}/${buildNumber}/*.war ."
+             }
+         }
+     }
+}
